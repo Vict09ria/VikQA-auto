b663f46d8b95ba371e9fe9d9a6156821894fe04e
Vict09ria <166547298+Vict09ria@users.noreply.github.com>
2024-05-15 13:36:26 +0300

    Formed structure

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -1,4 +1,8 @@
+import pytest
+
+
 class User:
+
     def __init__(self) -> None:
         self.name = None
         self.second_name = None
@@ -11,16 +15,20 @@ class User:
         self.name = " "
         self.second_name = " "
 
-    def test_change_name():
-        user = User()
-        user.create()
 
-        assert user.name == "Sergii"
-        user.remove()
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
 
-    def test_change_second_name():
-        user = User()
-        user.create()
+    yield user
 
-        assert user.second_name == "Butenko"
-        user.remove()
+    user.remove()
+
+
+def test_change_name(user):
+    assert user.name == "Sergii"
+
+
+def test_change_second_name(user):
+    assert user.second_name == "Butenko"
